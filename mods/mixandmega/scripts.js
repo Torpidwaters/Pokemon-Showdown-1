xfix/Pokemon-Showdown

commit c68aff18c7cc320b7992086a5c4054f9db83d8a9

Fix crashes in Mix and Mega (#112)
Replaced sugar.js object methods

mods/mixandmega/scripts.js
@@ -68,9 +68,9 @@ exports.BattleScripts = {
 	doGetMixedTemplate: function (template, deltas) {
 		if (!deltas) throw new TypeError("Must specify deltas!");
 		if (!template || typeof template === 'string') template = this.getTemplate(template);
-		template = Object.clone(template); // shallow is enough
+		template = Object.assign({}, template); // shallow is enough
 		template.abilities = {'0': deltas.ability};
-		template.types = Object.merge(template.types.slice(), deltas.types).compact().unique();
+		template.types = Object.assign(template.types.slice(), deltas.types).compact().unique();
 
 		let baseStats = template.baseStats;
 		template.baseStats = {};

Torpidwaters/Pokemon-Showdown-1

commit 243388a833854e059ab2d6c8504942ff759206c6

Mix and Mega: Fix a crash
I'm not particularly sure why this was different than main, but it was broken from https://github.com/jd4564/Pokemon-Showdown/commit/c68aff18c7cc320b7992086a5c4054f9db83d8a9 - so this fixes it.

mods/mixandmega/scripts.js
@@ -70,8 +70,11 @@ exports.BattleScripts = {
 		if (!template || typeof template === 'string') template = this.getTemplate(template);
 		template = Object.assign({}, template); // shallow is enough
 		template.abilities = {'0': deltas.ability};
-		template.types = Object.assign(template.types.slice(), deltas.types).compact().unique();
-
+		if (template.types[0] === deltas.type) {
+			template.types = [deltas.type];
+		} else if (deltas.type) {
+			template.types = [template.types[0], deltas.type];
+		}
 		let baseStats = template.baseStats;
 		template.baseStats = {};
 		for (let statName in baseStats) template.baseStats[statName] = baseStats[statName] + deltas.baseStats[statName];