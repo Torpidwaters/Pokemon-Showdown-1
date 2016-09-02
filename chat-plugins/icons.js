beeb5240ee344b8bcdeeaac54c5cf68ebc1805f4
Torpidwaters <jhonson404@gmail.com>
2016-09-02 14:31:07 -0700

    Use a more standard command format for /icon (#162)

--- chat-plugins/icons.js
+++ chat-plugins/icons.js
@@ -44,33 +44,36 @@ function generateCSS(name, icon) {
 
 exports.commands = {
 	customicon: 'icon',
-	icon: function (target, room, user) {
-		if (!this.can('customicon')) return false;
-		target = target.split(',');
-		for (let u in target) target[u] = target[u].trim();
-		if (!target[1]) return this.parse('/help icon');
-		if (toId(target[0]).length > 19) return this.errorReply("Usernames are not this long...");
-		if (target[1] === 'delete') {
-			if (!icons[toId(target[0])]) return this.errorReply('/icon - ' + target[0] + ' does not have an icon.');
-			delete icons[toId(target[0])];
-			updateIcons();
-			this.sendReply("You removed " + target[0] + "'s icon.");
-			Rooms('upperstaff').add(user.name + " removed " + target[0] + "'s icon.").update();
-			this.privateModCommand("(" + target[0] + "'s icon was removed by " + user.name + ".)");
-			if (Users(target[0]) && Users(target[0]).connected) Users(target[0]).popup("|modal||html|" + Wisp.nameColor(user.name, true) + " removed your icon.");
-			return;
-		}
+	icon: {
+		set: function (target, room, user) {
+			if (!this.can('customicon')) return false;
+			target = target.split(',');
+			for (let u in target) target[u] = target[u].trim();
+			if (!target[1]) return this.parse('/help icon');
+			if (toId(target[0]).length > 19) return this.errorReply("Usernames are not this long...");
 
-		this.sendReply("|raw|You have given <b><font color=" + Wisp.hashColor(Tools.escapeHTML(target[0])) + ">" + Tools.escapeHTML(target[0]) + "</font></b> an icon.");
-		if (Users(target[0]) && Users(target[0]).connected) Users(target[0]).popup("|modal||html|" + Wisp.nameColor(user.name, true) + " has set your icon as : <img src='" + target[1] + "' width='32' height='32'");
-		Rooms('upperstaff').add('|raw|<b><font color="' + Wisp.hashColor(Tools.escapeHTML(target[0])) + '">' + Tools.escapeHTML(target[0]) + '</font> has received an icon from ' + Tools.escapeHTML(user.name) + '.</b>').update();
-		this.privateModCommand("(" + target[0] + " has recieved icon: '" + target[1] + "' from " + user.name + ".)");
-		icons[toId(target[0])] = target[1];
-		updateIcons();
+			this.sendReply("|raw|You have given <b><font color=" + Wisp.hashColor(Tools.escapeHTML(target[0])) + ">" + Tools.escapeHTML(target[0]) + "</font></b> an icon.");
+			if (Users(target[0]) && Users(target[0]).connected) Users(target[0]).popup("|modal||html|" + Wisp.nameColor(user.name, true) + " has set your icon as : <img src='" + target[1] + "' width='32' height='32'");
+			Rooms('upperstaff').add('|raw|<b><font color="' + Wisp.hashColor(Tools.escapeHTML(target[0])) + '">' + Tools.escapeHTML(target[0]) + '</font> has received an icon from ' + Tools.escapeHTML(user.name) + '.</b>').update();
+			this.privateModCommand("(" + target[0] + " has recieved icon: '" + target[1] + "' from " + user.name + ".)");
+			icons[toId(target[0])] = target[1];
+			updateIcons();
+		},
+		delete: function (target, room, user) {
+			if (!this.can('customicon')) return false;
+			if (!icons[toId(target)]) return this.errorReply('/icon - ' + target + ' does not have an icon.');
+			delete icons[toId(target)];
+			updateIcons();
+			this.sendReply("You removed " + target + "'s icon.");
+			Rooms('upperstaff').add(user.name + " removed " + target + "'s icon.").update();
+			this.privateModCommand("(" + target + "'s icon was removed by " + user.name + ".)");
+			if (Users(target) && Users(target).connected) Users(target).popup("|modal||html|" + Wisp.nameColor(user.name, true) + " removed your icon.");
+			return;
+		},
 	},
 	iconhelp: [
 		"Commands Include:",
-		"/icon [user], [image url] - Gives [user] an icon of [image url]",
-		"/icon [user], delete - Deletes a user's icon",
+		"/icon set [user], [image url] - Gives [user] an icon of [image url]",
+		"/icon delete [user] - Deletes a user's icon",
 	],
 };
